5e8c04e80ee0d5c5d26c5cd73670be18722e5507
Pritam Das <dpritam28577@gmail.com>
2023-07-16 14:07:16 +0530

    base auth creation

--- routes/auth.js
+++ routes/auth.js
@@ -2,3 +2,10 @@ const express = require('express');
 
 const route = express.Router();
 
+route.get('/signup',()=>{})
+
+route.post('/signup',()=>{})
+
+route.get('/login',()=>{})
+
+route.post('/login',()=>{})
