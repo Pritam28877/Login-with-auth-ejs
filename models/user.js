c5702d1a8a895d468f997b2972fc32c32cc17837
Pritam Das <dpritam28577@gmail.com>
2023-07-18 17:53:49 +0530

    the use can add to the data base

--- models/user.js
+++ models/user.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String ,
-        required:true,
-        unique:true,
-        lowercase:true
-    },
-    password:{
-        type:String ,
-        required:true,
-        minLength:6
-    }
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 6,
+  }
 });
 
-const user = mongoose.Schema('user',userSchema);
+const User = mongoose.model('user', userSchema);
 
-module.exports = user;
+module.exports = User;
