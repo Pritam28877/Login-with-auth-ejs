8a23970efbf040eaca6598753e228729703bc3ab
Pritam Das <dpritam28577@gmail.com>
2023-07-19 21:34:47 +0530

    add the login function

--- models/user.js
+++ models/user.js
@@ -5,26 +5,38 @@ const bcrypt = require('bcrypt');
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
-    required: [true , 'Please enter an email'],
+    required: [true, 'Please enter an email'],
     unique: true,
     lowercase: true,
-    validate:[isEmail,'Please enter a valid email']
+    validate: [isEmail, 'Please enter a valid email']
   },
   password: {
     type: String,
-    required: [true , 'Please enter an passWord'],
-    minlength: [6 , 'Minimum password length is 6 characters'],
+    required: [true, 'Please enter an passWord'],
+    minlength: [6, 'Minimum password length is 6 characters'],
   }
 });
 
 //fire a function before doc saved to db the hashing 
 
-userSchema.pre('save', async function(next){
+userSchema.pre('save', async function (next) {
   const salt = await bcrypt.genSalt();
-  this.password = await bcrypt.hash(this.password , salt);
+  this.password = await bcrypt.hash(this.password, salt);
 
   next();
 })
+
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({ email });
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user;
+    }
+    throw Error('incorrect password');
+  }
+  throw Error('incorrect email');
+};
 const User = mongoose.model('user', userSchema);
 
 module.exports = User;
