eb4761f2f5a6a7af0b9db28d3fa1412ace5cbbf1
Pritam Das <dpritam28577@gmail.com>
2023-07-19 10:48:27 +0530

    handle customize errors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,31 +1,43 @@
-const User = require("../models/User");
+const User = require("../models/user");
+
+//handel errors 
+const handleErrors = (err) => {
+  // console.log(err.message, err.code);
+  let errors = { email: '', password: '' }
+  if(err.message.includes('user validation failed')){
+    (Object.values(err.errors)).forEach(properties=>{
+      errors[properties.path] = properties.message;
+    });
+  }
+    return errors ;
+}
 
 
 // controller actions
 module.exports.signup_get = (req, res) => {
-    res.render('signup');
+  res.render('signup');
 }
 
 module.exports.login_get = (req, res) => {
-    res.render('login');
+  res.render('login');
 }
 
 module.exports.signup_post = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-      const user = await User.create({ email, password });
-      res.status(201).json(user);
-    }
-    catch(err) {
-      console.log(err);
-      res.status(400).send('error, user not created');
-    }
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  }
+  catch (err) {
+    const error = handleErrors(err);
+    res.status(400).json({error});
+  }
 }
 
 module.exports.login_post = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    console.log(email , password)
-    res.send('user login');
+  console.log(email, password)
+  res.send('user login');
 }
