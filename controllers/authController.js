1bc68d061043d5bebbb12a1b20b43126fc5d926c
Pritam Das <dpritam28577@gmail.com>
2023-07-20 13:49:09 +0530

    add the error function on the login

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,11 +5,24 @@ const jwt = require('jsonwebtoken');
 const handleErrors = (err) => {
   // console.log(err.message, err.code);
   let errors = { email: '', password: '' }
+  // email is already registered
   if (err.code === 11000) {
     errors.email = 'that email is already registered';
     console.log(errors.email);
     return errors;
   }
+  // incorrect email
+  if (err.message === 'incorrect email') {
+    errors.email = 'That email is not registered';
+  }
+
+  // incorrect password
+  if (err.message === 'incorrect password') {
+    errors.password = 'That password is incorrect';
+  }
+
+
+  //duplicare error code
   if (err.message.includes('user validation failed')) {
     (Object.values(err.errors)).forEach(properties => {
       errors[properties.path] = properties.message;
@@ -54,8 +67,11 @@ module.exports.login_post = async (req, res) => {
 
   try {
     const user = await User.login(email, password);
+    const token = createToke(user._id);
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxage * 1000 })
     res.status(200).json({ user: user._id });
   } catch (err) {
-    res.status(400).json({});
+    const errors = handleErrors(err)
+    res.status(400).json({ errors });
   }
 }
